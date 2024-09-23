f78a5a94e03474da45994952feeaf72617694813
alikatteRajakala <132541608+alikatteRajakala@users.noreply.github.com>
2024-09-23 13:25:16 +0530

    addition function added

---
+++ demo.py
@@ -0,0 +1,8 @@
+def addition():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    return a + b
+
+# Example usage
+result = addition()
+print("The sum is:", result)
